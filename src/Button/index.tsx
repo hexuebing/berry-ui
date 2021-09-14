ed44819de955d9e2cb28ec18c0be47623a018e15
hexuebing <839308443@qq.com>
2021-09-14 18:54:41 +0800

    but

--- src/Button/index.tsx
+++ src/Button/index.tsx
@@ -1,5 +1,21 @@
 import React from 'react';
 import './index.css';
+import { tuple } from '@/_util/type';
+import { SizeType } from '@/config-provider/SizeContext';
+
+const ButtonTypes = tuple(
+  'default',
+  'primary',
+  'ghost',
+  'dashed',
+  'link',
+  'text',
+);
+export type ButtonType = typeof ButtonTypes[number];
+const ButtonShapes = tuple('circle', 'round');
+export type ButtonShape = typeof ButtonShapes[number];
+const ButtonHTMLTypes = tuple('submit', 'button', 'reset');
+export type ButtonHTMLType = typeof ButtonHTMLTypes[number];
 
 interface ButtonOptions {
   title: string;
@@ -7,12 +23,38 @@ interface ButtonOptions {
   onClick: () => void;
 }
 
-export default ({ title, href, onClick }: ButtonOptions) => (
-  <a className="light-btn" href={href} onClick={onClick}>
-    <span></span>
-    <span></span>
-    <span></span>
-    <span></span>
-    {title}
-  </a>
-);
+export interface BaseButtonProps {
+  type?: ButtonType;
+  icon?: React.ReactNode;
+  shape?: ButtonShape;
+  size?: SizeType;
+  loading?: boolean | { delay?: number };
+  prefixCls?: string;
+  className?: string;
+  ghost?: boolean;
+  danger?: boolean;
+  block?: boolean;
+  children?: React.ReactNode;
+}
+
+export type ButtonProps = Partial<BaseButtonProps & ButtonOptions>;
+
+const InternalButton: React.ForwardRefRenderFunction<unknown, ButtonProps> = (
+  props,
+  ref,
+) => {
+  const { href, onClick, title } = props;
+  return (
+    <a className="light-btn" href={href} onClick={onClick}>
+      <span></span>
+      <span></span>
+      <span></span>
+      <span></span>
+      {title}
+    </a>
+  );
+};
+
+const Button = React.forwardRef<unknown, ButtonProps>(InternalButton);
+
+export default Button;
